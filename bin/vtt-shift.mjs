98d1cf178305b04c688f7ddf207796c61a49abc6
Alhadis <gardnerjohng@gmail.com>
2019-06-13 04:29:01 +1000

    Replace test-script with a more useful executable

---
+++ bin/vtt-shift.mjs
@@ -0,0 +1,54 @@
+#!/usr/bin/env node --experimental-modules
+"use strict";
+
+import {readFileSync, writeFileSync} from "fs";
+import {basename} from "path";
+import utils      from "alhadis.utils";
+import getOpts    from "get-options";
+import SubripText from "../srt.mjs";
+
+const {options, argv} = getOpts(process.argv.slice(2), {
+	"-e, --encoding": "<string>",
+}, {
+	noUndefined: true,
+	noMixedOrder: true,
+	terminator: /^--$|^-\.?\d/,
+});
+
+const encoding = options.encoding || "utf8";
+const amount   = utils.parseCSSDuration(argv.pop());
+
+// Invalid offset amount, or not enough arguments
+if(Number.isNaN(amount) || !argv.length && process.stdin.isTTY){
+	const $0 = basename(process.argv[1]);
+	process.stderr.write(`Usage: ${$0} [-e encoding] files ... offset\n`);
+	process.exit(1);
+}
+
+// Modify files in-place
+if(argv.length)
+	for(const file of argv){
+		const input  = readFileSync(file, {encoding});
+		const vtt    = new SubripText(input);
+		vtt.offset(amount);
+		
+		// Only write back if contents differ
+		const result = vtt.toString();
+		if(result !== input)
+			writeFileSync(file, result);
+	}
+
+// Read from standard input
+else if(!process.stdin.isTTY)
+	new Promise(resolve => {
+		let input = "";
+		process.stdin.setEncoding(encoding);
+		process.stdin.on("readable", () => {
+			const chunk = process.stdin.read();
+			null !== chunk ? input += chunk : resolve(input);
+		});
+	}).then(input => {
+		const vtt = new SubripText(input);
+		vtt.offset(amount);
+		process.stdout.write(vtt.toString());
+	});
